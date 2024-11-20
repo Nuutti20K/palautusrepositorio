abd8043e2e3efb4871710c76b29816886fb998ae
Nuutti Kekkonen <nuutti.kekkonen@saunalahti.fi>
2024-11-20 12:50:23 +0200

    Tehtävät 1-4 tehty

---
+++ viikko4/verkkokauppa/src/ostoskori.py
@@ -0,0 +1,21 @@
+class Ostoskori:
+    def __init__(self):
+        self._tuotteet = []
+    
+    def lisaa(self, tuote):
+        self._tuotteet.append(tuote)
+    
+    def poista(self, tuote):
+        for asia in self._tuotteet:
+            if asia.id == tuote.id:
+                self._tuotteet.remove(asia)
+                return
+
+        #self._tuotteet = list(
+        #    filter(lambda t: t.id != tuote.id, self._tuotteet)
+        #)
+
+    def hinta(self):
+        hinnat = map(lambda t: t.hinta, self._tuotteet)
+
+        return sum(hinnat)
